a094d2ab192cb8f6797dbfa56f1ebbac762f0af2
Jeremy Templier <templier.jeremy@gmail.com>
2013-06-03 00:35:50 -0700

    bug fix convert of dumped view to an html safe string

--- ShakeReport/Library/NSString+HTML.m
+++ ShakeReport/Library/NSString+HTML.m
@@ -13,9 +13,43 @@
 
 - (NSString*)toHTML
 {
-    NSString *htmlString = [self stringByReplacingOccurrencesOfString:@"\n" withString:@"<br/>"];
+    NSString *htmlString = [self toHTMLSafe];
+    htmlString = [htmlString stringByReplacingOccurrencesOfString:@"\n" withString:@"<br/>"];
     htmlString = [htmlString stringByReplacingOccurrencesOfString:@"\t" withString:@"&emsp;&emsp;&emsp;&emsp;"];
     return htmlString;
+}
+
+- (NSString *)toHTMLSafe {
+    NSMutableString *encoded = [NSMutableString stringWithString:self];
+    
+    // @"&amp;"
+    NSRange range = [self rangeOfString:@"&"];
+    if (range.location != NSNotFound) {
+        [encoded replaceOccurrencesOfString:@"&"
+                                 withString:@"&amp;"
+                                    options:NSLiteralSearch
+                                      range:NSMakeRange(0, [encoded length])];
+    }
+    
+    // @"&lt;"
+    range = [self rangeOfString:@"<"];
+    if (range.location != NSNotFound) {
+        [encoded replaceOccurrencesOfString:@"<"
+                                 withString:@"&lt;"
+                                    options:NSLiteralSearch
+                                      range:NSMakeRange(0, [encoded length])];
+    }
+    
+    // @"&gt;"
+    range = [self rangeOfString:@">"];
+    if (range.location != NSNotFound) {
+        [encoded replaceOccurrencesOfString:@">"
+                                 withString:@"&gt;"
+                                    options:NSLiteralSearch
+                                      range:NSMakeRange(0, [encoded length])];
+    }
+    
+    return encoded;
 }
 
 @end
